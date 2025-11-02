f6a33ee8bdbe796a54ba30ba96f8caaf03efb71c
Craft146 <danila.zenkovitch17@yandex.ru>
2025-11-02 21:19:36 +0300

    add conftest.py, engine_null_pool and test_db.py

--- tests/integration_tests/role/test_db.py
+++ tests/integration_tests/role/test_db.py
@@ -6,6 +6,5 @@ from src.schemas.role import RoleAdd
 async def test_create_role():
     role_data = RoleAdd(title="admin")
     async with async_session_maker() as session:
-        role = await RoleRepository(session).add(role_data)
-
-        print(f"{role=}")
+        await RoleRepository(session).add(role_data)
+        await session.commit()
